6efa555b034e63c4b16995c0369f8e260e527260
tuomaxu <you@example.com>
2019-01-15 10:30:27 +0800

    构建应用基本功能框架

--- main.c
+++ main.c
@@ -1,9 +1,40 @@
 #include <stdio.h>
 
 int main(){
+    printf("进入钱包管理系统\n");
 
-    printf("hello world!");
+    
+    while(1){
+
+        printf("1---存钱\n");
+        printf("2---取钱\n");
+        printf("3---余额\n");
+        printf("4---退出\n");
+
+        printf("请选择相应功能\n");
+        int code;
+        scanf("%d",&code);
+
+        if(code == 1){
+
+        }
+
+        if(code == 2){
+            
+        }
+
+        if(code == 3){
+            
+        }
+
+        if(code == 4){
+            
+        }
+
+
+    }
+    
+
 
     return 0;
-
 }
